9a944e3fcb7e66f82100301d5c452b429ae3c720
CauaneTemporin <temporin.cauane@gmail.com>
2023-03-14 16:49:26 -0300

    Projeto criado

---
+++ src/main/java/com/temporincauane/CrudSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.temporincauane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temporincauane/CrudSpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temporincauane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
